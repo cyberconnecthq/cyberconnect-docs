b521f5e89a52c826e08d93bb2b18f6678a03ea38
Ryan Li <ryan@ryanli.me>
2022-01-13 14:41:45 -0800

    init

---
+++ docs/introduction.md
@@ -0,0 +1,44 @@
+---
+sidebar_position: 1
+id: introduction
+---
+
+# What is CyberConnect
+## Protocol and Interface
+To begin, we should make clear the distinctions between the different areas of "CyberConnect", some of which may confuse new users.
+- CyberConnect Protocol: A suite of infrastructure and tools including decentralized identity (DID), data storage and data indexing service that together democratize social connections, a protocol that facilitates the formation of platformless, user-centralic social graph.
+- CyberConnect Interface: A web interface that allows for easy interaction with the CyberConnect protocol. The interface is only one of many ways one may interact with the CyberConnect protocol.
+
+The following is a brief overview of the *CyberConnect* protocol.
+
+
+## Introduction
+
+The CyberConnect protocol is a peer-to-peer system designed for the creation and storage of user-centric, self-soverign social graph and identity on any blockchain. The protocol is implmenented through a suite of components including decentralized content computation, dicentralized identity system and a distributed indexing and query engine; desigend to prioritize censorship resistance, self-sovereignty and to operate in a platformless, blockchain-agnostic manager.
+
+### What is **Social Graph**?
+
+By definition, the social graph is a graph that represents social relations between entities. 
+
+
+You social graph 
+
+An address book that maps social relations between individuals  
+A powerful means to position an individual in a complex network of connections, organizations, affiliations, which define his/her social existence
+
+
+
+It could present itself in different forms: An address book that maps social relations between individuals through names and phone numbers; Friend list or follower list on social platforms, defining the user's social existence among their among their network.
+
+
+
+A powerful means to position an individual in a complex network of connections, organizations, affiliations, which define his/her social existence
+Examples: Facebook’s friend list, Twitter’s follower list, Mobile phone contact book
+
+
+### How does CyberConnect protocol compare to a social network
+
+People never get to 
+
+
+exchanging cryptocurrencies (ERC-20 Tokens) on the Ethereum blockchain. The protocol is implemented as a set of persistent, non-upgradable smart contracts; designed to prioritize censorship resistance, security, self-custody, and to function without any trusted intermediaries who may selectively restrict access.
---
+++ docusaurus.config.js
@@ -0,0 +1,117 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'CyberConnect',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'CyberConnect Docs',
+        logo: {
+          alt: 'CyberConnect Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Introduction',
+          },
+          // {
+          //   type: 'doc',
+          //   docId: 'sdk',
+          //   position: 'left',
+          //   label: 'SDK',
+          // },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} CyberConnect, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
