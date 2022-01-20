d08df63b0b5d94b296275702d1f61043b528b5c8
WildSaoFeng <wildsaofeng@gmail.com>
2022-01-20 15:18:11 +0800

    fix broken link issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/introduction',
               },
             ],
           },
@@ -121,10 +121,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
